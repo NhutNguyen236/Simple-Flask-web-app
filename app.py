b01638adff28bcfea96d1ca267e0054e714f448e
NhutNguyen236 <nhutnguyenf330@gmail.com>
2022-06-07 13:43:10 +0700

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+# create flask app
+app = Flask(__name__)
+app.secret_key = "hello world shinobi"
+
+# greeting route
+
+
+@app.route('/')
+def index():
+    # get flash messages
+    flash("Tell me your name?")
+    # render template
+    return render_template('index.html')
+
+
+@app.route("/hello", methods=['POST', 'GET'])
+def greeter():
+    flash("🥥 Hello " + str(request.form['name_input']) + " 🌴")
+    return render_template('index.html')
---
+++ readme.md
@@ -0,0 +1,9 @@
+![ss](/assets/img/deno.png)
+
+# References
+- Simple Flask app by [Python Simplified](youtube.com/watch?v=6plVs_ytIH8)
+
+# My features on this tut
+- Used [Static assets storing](https://www.codegrepper.com/code-examples/python/flask+static+images) instead of url access
+- Changed default route to "/"
+- My own css styling, this is normal tho :D 
